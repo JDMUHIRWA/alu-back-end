87b0627ad82dd0dd433e58f591ecd2808ac30f41
JDMUHIRWA <j.hareriman@alustudent.com>
2024-06-26 12:26:25 +0200

    gathering data from an API

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
 """
-a Python script that, using REST API,
-returns information about employee TODO list progress.
+A Python script that, using a REST API,
+returns information about an employee's TODO list progress.
 """
 if __name__ == "__main__":
     import json
     import sys
     import urllib.request
 
-    """
-    format the employees id with the url
-    """
+    # Format the employee ID with the URL
     employee_id = sys.argv[1]
     url1 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     url2 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
+    
+    # Request TODO list data
     req_object1 = urllib.request.Request(url1, method="GET")
-    req_object2 = urllib.request.Request(url2, method="GET")
     with urllib.request.urlopen(req_object1) as response_object1:
         response1 = json.load(response_object1)
+    
+    # Request employee data
+    req_object2 = urllib.request.Request(url2, method="GET")
     with urllib.request.urlopen(req_object2) as response_object2:
         response2 = json.load(response_object2)
-    completed_tasks = []
-    for task in response1:
-        if task['completed'] is not True:
-            continue
-        completed_tasks.append(task)
+    
+    # Process completed tasks
+    completed_tasks = [task for task in response1 if task['completed']]
     no_of_comptasks = len(completed_tasks)
     totalno_of_task = len(response1)
+    
+    # Get employee name
     employee_name = response2["name"]
-    print(f"Employee {employee_name} is done with tasks({no_of_comptasks}/\
-{totalno_of_task}):")
-    for comp_tasks in completed_tasks:
-        print(f"\t {comp_tasks['title']}")
+    
+    # Print the TODO list progress
+    print(f"Employee {employee_name} is done with tasks({no_of_comptasks}/{totalno_of_task}):")
+    for comp_task in completed_tasks:
+        print(f"\t {comp_task['title']}")
